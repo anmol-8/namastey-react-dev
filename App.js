a79523fb069e63b00a3b2e4b495b85bcbb187d77
SHARMA <anmol.sharma.ext@siemens.com>
2023-09-08 22:57:52 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,4 +1,34 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import Header from "./Header";
 
-const heading = React.createElement("div",{id: "heading" , xyz: "zyz"} , "Hello World using React" );
+const App = () => {
+     return (
+        <div>
+            <Header></Header>
+        </div>
+     )
+} 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<App />)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading = React.createElement("div",{id: "heading" , xyz: "zyz"} , "Hello World using React" );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
